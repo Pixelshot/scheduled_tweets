634b4da99d1af477f429f66238c58c8d1fd50453
Pixelshot <pixelshot8@gmail.com>
2021-08-10 17:40:09 +0800

    Connected sign up form to User model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,24 @@ class RegistrationsController < ApplicationController
     # In ruby if we don't use instance variable(@), the variable will be wiped out and won't be shown in views. Refer to part 13 for more info.
     @user = User.new  
   end
+
+  def create
+    # Part 14 explains params really well
+    # render plain: params -> This prints out the whole object with their params
+    # render plain: params[:user] -> This prints out user object with its params 
+    # To create a new user in our database, we'll use params[:user] and insert its data into our model
+    # This should be done under private section below to make it secure
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
